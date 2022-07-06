cb2f1d40a78c6e50dcc1576c45f1509084c442b7
James Alhassan <jamesalhassan052@gmail.com>
2022-07-06 23:40:24 +0100

    IMPLICIT TYPE CONVERSION

---
+++ app.js
@@ -0,0 +1,29 @@
+// IMPLICIT TYPE CONVERSION
+
+const numOne = "john";
+const numTwo = "kofi";
+
+// Substracting Two Non-digit Strings
+const result = numOne - numTwo;
+console.log(result);
+// NaN
+
+const numThree = "10";
+const numFour = "20";
+
+// Substracting Two number Strings
+const resultTwo = numThree - numFour;
+
+console.log(resultTwo);
+//  -10
+
+//  Adding two number strings will be concatenation
+const resultThree = numFour + numThree;
+console.log(resultThree);
+//  1020
+
+const numFive = 10;
+const resultFour = numThree + numFive;
+
+console.log(resultFive);
+// 1010
