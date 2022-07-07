fdf1a88e0f283ed3461c7608c376ba400d408fb6
James Alhassan <jamesalhassan052@gmail.com>
2022-07-07 08:33:31 +0100

    while and do while

--- app.js
+++ app.js
@@ -32,56 +32,72 @@ const resultFour = numThree + numFive;
 // Primitive - strings, number, boolean, null, undefined, symbol
 // Object - Arrays, Functions, objects
 
-function addvalue(num1, num2) {
-  return num1 + num2;
-}
-const value = addvalue(2, 3);
-// FUNCTION EXPRESSION/ Anonymous functions
-const multiply = function (num1, num2) {
-  return num1 * num2;
-};
-const values = [10, 20, multiply(10, 20)];
+// function addvalue(num1, num2) {
+//   return num1 + num2;
+// }
+// const value = addvalue(2, 3);
+// // FUNCTION EXPRESSION/ Anonymous functions
+// const multiply = function (num1, num2) {
+//   return num1 * num2;
+// };
+// const values = [10, 20, multiply(10, 20)];
 
 // COMPARISON OPERATORS
 // ==  checks only value whilst  === checks for value and type
 
-const a = 10;
-const b = 10;
-const c = "10";
+// const a = 10;
+// const b = 10;
+// const c = "10";
 
-if (a == b) {
-  console.log("true");
-}
-if (a == c) {
-  console.log("true");
-}
+// if (a == b) {
+//   console.log("true");
+// }
+// if (a == c) {
+//   console.log("true");
+// }
 
-if (a === "c") {
-  console.log("false");
-}
+// if (a === "c") {
+//   console.log("false");
+// }
 
 // SWITCH STATMENT
-const dice = 5;
+// const dice = 5;
 
-switch (dice) {
-  case 1:
-    console.log("you got one");
-    break;
-  case 2:
-    console.log("you got two");
-    break;
-  case 3:
-    console.log("you got three");
-    break;
-  case 4:
-    console.log("you got four");
-    break;
-  case 5:
-    console.log("you got five");
-    break;
-  case 6:
-    console.log("you got six");
-    break;
-  default:
-    console.log("you did not roll the dice");
+// switch (dice) {
+//   case 1:
+//     console.log("you got one");
+//     break;
+//   case 2:
+//     console.log("you got two");
+//     break;
+//   case 3:
+//     console.log("you got three");
+//     break;
+//   case 4:
+//     console.log("you got four");
+//     break;
+//   case 5:
+//     console.log("you got five");
+//     break;
+//   case 6:
+//     console.log("you got six");
+//     break;
+//   default:
+//     console.log("you did not roll the dice");
+// }
+
+// While and do while loops
+let amt = 10;
+
+while (amt > 1) {
+  console.log(`You have ${amt} in your account`);
+  amt--;
 }
+
+//  code comes before condition, runs at least once
+let money = 12;
+
+do {
+  console.log(`You have ${amt} in your account`);
+  money++;
+} while (money < 10);
